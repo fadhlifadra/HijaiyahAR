c18502b0587f76927f03ae598ddabeb4bc8e541f
Muhammad Fadhli Hisyam <38355018+fadhlifadra@users.noreply.github.com>
2020-05-03 09:11:27 +0700

    tes

---
+++ Assets/Script/mainmenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenu : MonoBehaviour
+{
+    public void GoMain()
+    {
+        Application.LoadLevel("PlayGame");
+    }
+
+    public void GoAbout()
+    {
+        Application.LoadLevel("AboutUs");
+    }
+
+    public void GoHow()
+    {
+        Application.LoadLevel("CaraBermain");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Make sure user is on Android platform
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            // Check if Back was pressed this frame
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                // Quit the application
+                Application.Quit();
+            }
+        }
+    }
+}
