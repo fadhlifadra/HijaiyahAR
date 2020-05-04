ad27fc1b25061a196227fa57aeaf8c7b0331b293
Muhammad Fadhli Hisyam <38355018+fadhlifadra@users.noreply.github.com>
2020-05-04 15:56:24 +0700

    dikit lagi jadi

---
+++ Assets/Script/menu_control.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu_control : MonoBehaviour
+{
+    public GameObject confirm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        confirm.SetActive(false);
+    }
+
+    public void GoBack()
+    {
+        confirm.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/playgame_control.cs
+++ Assets/Script/playgame_control.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class playgame_control : MonoBehaviour
 {
+    public GameObject alif, ba, ta, tsa, jim, kha, kho, dal, dzal, ra, za, sin, syin, shad, dhad, tha, dha, ain, ghain, fa, qaf, kaf, lam, mim, nun, wau, hamzah, ha, ya;
     // Start is called before the first frame update
     void Start()
     {
-        
+        alif.SetActive(true);
+        ba.SetActive(false);
+        ta.SetActive(false);
+        tsa.SetActive(false);
+        jim.SetActive(false);
+        kha.SetActive(false);
+        kho.SetActive(false);
+        dal.SetActive(false);
+        dzal.SetActive(false);
+        ra.SetActive(false);
+        za.SetActive(false);
+        sin.SetActive(false);
+        syin.SetActive(false);
+        shad.SetActive(false);
+        dhad.SetActive(false);
+        tha.SetActive(false);
+        dha.SetActive(false);
+        ain.SetActive(false);
+        ghain.SetActive(false);
+        fa.SetActive(false);
+        qaf.SetActive(false);
+        kaf.SetActive(false);
+        lam.SetActive(false);
+        mim.SetActive(false);
+        nun.SetActive(false);
+        wau.SetActive(false);
+        hamzah.SetActive(false);
+        ha.SetActive(false);
+        ya.SetActive(false);
     }
 
     // Update is called once per frame
